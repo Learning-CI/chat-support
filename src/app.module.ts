c9f957a275fcc639271c96d36b7fdacf68af1a60
Vitor Aderaldo <vitor.aldero@harmonize.health>
2023-01-17 01:43:52 -0300

    feat: create training service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './api/app.controller';
-//import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'user',
+      password: 'password',
+      database: 'chat-support',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   //providers: [AppService],
 })
---
+++ src/domain/bot-training/repository/training-history.repository.ts
@@ -0,0 +1,4 @@
+import { Repository } from '../../../@shared/repository/repository.interface';
+import { TrainingHistory } from '../entity/training-history';
+
+export type TrainingHistoryRepository = Repository<TrainingHistory>;
